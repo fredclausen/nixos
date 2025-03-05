455f4bcf2431a7c7b1aa3fb20a52b6caa1c4979e
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-03-05 12:10:26 -0700

    Update shell.nix

--- common/shell.nix
+++ common/shell.nix
@@ -2,7 +2,6 @@
 {
   config = {
     programs.zsh.enable = true;
-    programs.bash.enable = true;
 
     # Define a user account. Don't forget to set a password with ‘passwd’.
     users.defaultUserShell = pkgs.zsh;
