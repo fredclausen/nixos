c8171a526967df7b350f6e5dfd5b6800c64b32a6
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-03-17 08:46:08 -0600

    turn on airprint

--- packages/desktop/print/default.nix
+++ packages/desktop/print/default.nix
@@ -3,5 +3,11 @@
   config = {
     # Enable CUPS to print documents.
     services.printing.enable = true;
+
+    services.avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
   };
 }
