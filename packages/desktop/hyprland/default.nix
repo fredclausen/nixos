f57b3414e25eeb9ee9e15399a046c4e7dcea6b8f
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-03-19 14:11:10 -0600

    hyprland

--- packages/desktop/hyprland/default.nix
+++ packages/desktop/hyprland/default.nix
@@ -63,6 +63,41 @@ in
             "col.inactive_border" = "rgba(595959aa)";
           };
 
+          decoration = {
+            "rounding" = 10;
+            blur = {
+              enabled = true;
+              size = 3;
+              passes = 1;
+            };
+
+            # drop_shadow = "yes";
+            # shadow_range = 4;
+            # shadow_render_power = 3;
+            # "col.shadow" = "rgba(1a1a1aee)";
+          };
+
+          animations = {
+            enabled = "yes";
+
+            bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+
+            animation = [
+              "windows, 1, 7, myBezier"
+              "windowsOut, 1, 7, default, popin 80%"
+              "border, 1, 10, default"
+              "borderangle, 1, 8, default"
+              "fade, 1, 7, default"
+              "workspaces, 1, 6, default"
+            ];
+          };
+
+          binds = {
+            scroll_event_delay = 0;
+          };
+
+          #windowrulev2 = "nomaximizerequest, class:.*";
+
           bind = [
             "$mainMod, F, exec, firefox"
             "$mainMod, Q, exec, $terminal"
