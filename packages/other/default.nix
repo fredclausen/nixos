59ff815a2370de9126459e32ea562b20a971a18c
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-03-18 22:27:12 -0600

    add bat to home manager

--- packages/other/default.nix
+++ packages/other/default.nix
@@ -1,6 +1,10 @@
 {
   imports = [
+    ./bat
     ./lazygit
     ./yazi
   ];
+
+  # FIXME: bat and lazygit have a dot file in the dotfiles dir, but the config could not be imported from those files and instead
+  # we mirrored it here. Fix this to programmatically import the config from the dotfiles dir.
 }
--- users/fred/default.nix
+++ users/fred/default.nix
@@ -12,7 +12,6 @@
 
       packages = with pkgs; [
         fastfetch
-        bat
         gh
         fzf
         fd
