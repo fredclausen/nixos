b24d5fd3b435de98541d1c6937e3f4a4f5693a6e
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-03-20 14:44:40 -0600

    lock screen for hyprland

--- packages/desktop/hyprland/default.nix
+++ packages/desktop/hyprland/default.nix
@@ -39,6 +39,8 @@ in
         swaybg
         wlogout
         networkmanagerapplet
+        hypridle
+        hyprlock
       ];
     };
 
@@ -50,6 +52,67 @@ in
     };
 
     home-manager.users.fred = {
+      services.hypridle = {
+        enable = true;
+
+        settings = {
+          general = {
+            after_sleep_cmd = "hyprctl dispatch dpms on";
+            ignore_dbus_inhibit = false;
+            lock_cmd = "hyprlock";
+          };
+
+          listener = [
+            {
+              timeout = 900;
+              on-timeout = "hyprlock";
+            }
+            {
+              timeout = 1200;
+              on-timeout = "hyprctl dispatch dpms off";
+              on-resume = "hyprctl dispatch dpms on";
+            }
+          ];
+        };
+      };
+
+      programs.hyprlock = {
+        enable = true;
+
+        settings = {
+          general = {
+            disable_loading_bar = true;
+            grace = 5;
+            hide_cursor = true;
+            no_fade_in = false;
+          };
+
+          background = [
+            {
+              path = "/home/fred/GitHub/fred-config/lewis.jpg";
+              blur_passes = 3;
+              blur_size = 8;
+            }
+          ];
+
+          input-field = [
+            {
+              size = "200, 50";
+              position = "0, -80";
+              monitor = "";
+              dots_center = true;
+              fade_on_empty = false;
+              font_color = "rgb(202, 211, 245)";
+              inner_color = "rgb(91, 96, 120)";
+              outer_color = "rgb(24, 25, 38)";
+              outline_thickness = 5;
+              placeholder_text = "Password...";
+              shadow_passes = 2;
+            }
+          ];
+        };
+      };
+
       wayland.windowManager.hyprland = {
         enable = true;
 
@@ -74,6 +137,8 @@ in
             "hyprctl setcursor Adwaita 24"
             "swaybg -o \"*\" -i \"/home/fred/GitHub/fred-config/lewis.jpg\" &"
             "nm-applet --indicator"
+            "1password --silent &"
+            "hypridle"
           ];
 
           general = {
