2ff376e5e42edb1c03baa11a3c0794c13647ca68
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-04-01 19:24:23 -0600

    add selective screen shot

--- packages/desktop/hyprland/default.nix
+++ packages/desktop/hyprland/default.nix
@@ -285,6 +285,7 @@ in
             ", XF86MonBrightnessDown, exec, ~/.config/hyprextra/scripts/backlight --dec"
             ", XKB_KEY_XF86KbdBrightnessUp, exec, ~/.config/hyprextra/scripts/kbbacklight --inc"
             ", XKB_KEY_XF86KbdBrightnessDown, exec, ~/.config/hyprextra/scripts/kbbacklight --dec"
+            ", XF86SelectiveScreenshot, exec, grim -g \"$(slurp)\""
           ];
 
           bindm = [
