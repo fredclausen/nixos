64dd72b35f16b2f7371626f62c1eee5a580a1692
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-04-07 14:57:16 -0600

    updates

--- users/fred/default.nix
+++ users/fred/default.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
 {
   config = {
+    hardware.rtl-sdr.enable = true;
+    boot.kernelParams = [ "modprobe.blacklist=dvb_usb_rtl28xxu" ]; # blacklist this module
+
+    services.udev.packages = [ pkgs.rtl-sdr ]; # (there might be other packages that require udev here too)
+
     users.users.fred = {
       isNormalUser = true;
       description = "Fred Clausen";
@@ -8,6 +13,7 @@
         "networkmanager"
         "wheel"
         "docker"
+        "plugdev"
       ];
 
       packages = with pkgs; [
