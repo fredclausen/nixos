30ed4700b51c15a93930db8dde0d7ae443e6d261
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-04-07 11:09:03 -0600

    fix hm and nvim read only problem

--- packages/develop/nvim/default.nix
+++ packages/develop/nvim/default.nix
@@ -1,48 +1,48 @@
-{ config, pkgs, ... }:
 {
   config = {
-    home-manager.users.fred = {
-      home.packages = with pkgs; [
-        neovim
-        # packages needed for my nvim config to work
-        lua51Packages.lua
-        luajitPackages.luarocks_bootstrap
-        tree-sitter
-        # packages needed for lsp
-        rust-analyzer-unwrapped
-        bash-language-server
-        black
-        docker-compose-language-service
-        dockerfile-language-server-nodejs
-        eslint_d
-        gitui
-        hadolint
-        lua-language-server
-        luajitPackages.luacheck
-        github-markdown-toc-go
-        markdownlint-cli2
-        marksman
-        nodePackages.prettier
-        prettierd
-        pyright
-        ruff
-        selene
-        shellcheck
-        shellharden
-        shfmt
-        stylua
-        taplo
-        vscode-langservers-extracted
-        vscode-langservers-extracted
-        vtsls
-        yaml-language-server
-        copilot-node-server
-        nil
-      ];
+    home-manager.users.fred =
+      { config, pkgs, ... }:
+      {
+        home.packages = with pkgs; [
+          neovim
+          # packages needed for my nvim config to work
+          lua51Packages.lua
+          luajitPackages.luarocks_bootstrap
+          tree-sitter
+          # packages needed for lsp
+          rust-analyzer-unwrapped
+          bash-language-server
+          black
+          docker-compose-language-service
+          dockerfile-language-server-nodejs
+          eslint_d
+          gitui
+          hadolint
+          lua-language-server
+          luajitPackages.luacheck
+          github-markdown-toc-go
+          markdownlint-cli2
+          marksman
+          nodePackages.prettier
+          prettierd
+          pyright
+          ruff
+          selene
+          shellcheck
+          shellharden
+          shfmt
+          stylua
+          taplo
+          vscode-langservers-extracted
+          vscode-langservers-extracted
+          vtsls
+          yaml-language-server
+          copilot-node-server
+          nil
+        ];
 
-      xdg.configFile = {
-        "nvim/".source = ../../../dotfiles/fred/.config/nvim;
+        home.file."./.config/nvim".source =
+          config.lib.file.mkOutOfStoreSymlink "/home/fred/GitHub/nixos/dotfiles/fred/.config/nvim";
       };
-    };
   };
 }
