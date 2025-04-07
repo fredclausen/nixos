54d316fa4373241a4e53cff5d5ccad5efdb5e189
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-04-07 14:29:26 -0600

    acarshub system

---
+++ systems/acarshub/configuration.nix
@@ -0,0 +1,36 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.default
+    ../../packages
+    ../../users
+  ];
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  # extra options
+  desktop.enable = false;
+  desktop.enable_extra = false;
+  desktop.enable_games = false;
+  desktop.enable_streaming = false;
+
+  networking.hostName = "acarshub"; # Define your hostname.
+
+  environment.systemPackages = with pkgs; [
+  ];
+
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
