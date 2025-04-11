1b461f8f01dfa6a22f32a7432b510942df3242be
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-04-11 15:01:23 -0600

    bat to cat

--- packages/shell/bat/default.nix
+++ packages/shell/bat/default.nix
@@ -15,26 +15,7 @@
         enable = true;
       };
 
-      home.file.".config/bat/" = {
-        source = ../../../dotfiles/fred/.config/bat;
-        recursive = true;
-      };
-
-      home.file.".config/bat/themes/Catppuccin-mocha.tmTheme" = {
-        source = ../../../dotfiles/fred/.config/yazi/Catppuccin-mocha.tmTheme;
-      };
-      # themes = {
-      #     Catppuccin-mocha= {
-      #       src = builtins.readFile ../../../dotfiles/fred/.config/yazi/Catppuccin-mocha.tmTheme;
-      #       file = "Catppuccin-mocha.tmTheme";
-      #     };
-      #   };
-
-      #   config = {
-      #     theme = "Catppuccin-Mocha";
-      #     italic-text = "always";
-      #   };
-      # };
+      catppuccin.bat.enable = true;
     };
   };
 }
