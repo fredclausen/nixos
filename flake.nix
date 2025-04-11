4b9536b4a60aa1f108a3c6230187186590b79f3e
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-04-11 11:06:43 -0600

    add stylix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    stylix.url = "github:danth/stylix";
 
     home-manager = {
       url = "github:nix-community/home-manager";
