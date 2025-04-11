e4b7a4fbf188c06bffddc8f99ba76c129da2825e
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-04-11 14:27:21 -0600

    updates

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    stylix.url = "github:danth/stylix";
 
     home-manager = {
       url = "github:nix-community/home-manager";
