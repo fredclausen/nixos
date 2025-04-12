6cf370f49a331e80e19c2c436bc988d319b2041f
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-04-12 08:50:02 -0600

    cat lazygit

--- packages/shell/lazygit/default.nix
+++ packages/shell/lazygit/default.nix
@@ -13,33 +13,14 @@
 
       programs.lazygit = {
         enable = true;
-        # settings = {
-        #   gui = {
-        #     nerdFontsVersion = "3";
-        #     theme = {
-        #       activeBorderColor = [
-        #         "#a6d189"
-        #         "bold"
-        #       ];
-        #       inactiveBorderColor = [ "#a5adce" ];
-        #       optionsTextColor = [ "#8caaee" ];
-        #       selectedLineBgColor = [ "#414559" ];
-        #       cherryPickedCommitBgColor = [ "#51576d" ];
-        #       cherryPickedCommitFgColor = [ "#a6d189" ];
-        #       unstagedChangesColor = [ "#e78284" ];
-        #       defaultFgColor = [ "#c6d0f5" ];
-        #       searchingActiveBorderColor = [ "#e5c890" ];
-        #       #   authorColors = [
-        #       #     "Fred Clausen" = ["blue"]
-        #       #   ]
-        #     };
-        #   };
-        # };
+        settings = {
+          gui = {
+            nerdFontsVersion = "3";
+          };
+        };
       };
 
-      xdg.configFile = {
-        "lazygit/config.yml".source = ../../../dotfiles/fred/.config/lazygit/config.yml;
-      };
+      catppuccin.lazygit.enable = true;
     };
   };
 }
