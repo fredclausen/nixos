00210681ea8ef0df57c608e00e7c932b13c2a9fe
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-04-12 23:05:33 -0600

    theme nvim

--- dotfiles/fred/.config/nvim/lua/plugins/colorscheme.lua
+++ dotfiles/fred/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
     {
       "catppuccin/nvim",
+      priority = 1000,
       name = "catppuccin",
       lazy = false,
       config = function()
@@ -114,87 +115,88 @@ return {
     { "shaunsingh/oxocarbon.nvim", lazy = false },
     { "LunarVim/horizon.nvim", lazy = false },
     { "rose-pine/neovim", name = "rose-pine", lazy = false },
-    {
-      "tokyonight.nvim",
-      priority = 1000,
-      opts = function(_, _opts)
-        return {
-          style = "moon",
-          styles = {
-            keywords = { italic = false },
-            sidebars = "transparent",
-            floats = "transparent",
-          },
-          transparent = true,
-          sidebars = {
-            "qf",
-            "vista_kind",
-            "terminal",
-            "spectre_panel",
-            "NeogitStatus",
-            "startuptime",
-            "Outline",
-          },
-          ---@param hl Highlights
-          ---@param c ColorScheme
-          on_highlights = function(hl, c)
-            hl.CursorLineNr = { fg = c.orange, bold = true }
-            -- hl.LineNr = { fg = c.orange, bold = true }
-            hl.LineNrAbove = { fg = c.fg_gutter }
-            hl.LineNrBelow = { fg = c.fg_gutter }
-            local prompt = "#2d3149"
-            hl.TelescopeNormal = { bg = c.bg_dark, fg = c.fg_dark }
-            hl.TelescopeBorder = { bg = c.bg_dark, fg = c.bg_dark }
-            hl.TelescopePromptNormal = { bg = prompt }
-            hl.TelescopePromptBorder = { bg = prompt, fg = prompt }
-            hl.TelescopePromptTitle = { bg = c.fg_gutter, fg = c.orange }
-            hl.TelescopePreviewTitle = { bg = c.bg_dark, fg = c.bg_dark }
-            hl.TelescopeResultsTitle = { bg = c.bg_dark, fg = c.bg_dark }
 
-            hl.TSRainbowRed = { fg = c.red }
-            hl.TSRainbowYellow = { fg = c.yellow }
-            hl.TSRainbowBlue = { fg = c.blue }
-            hl.TSRainbowOrange = { fg = c.orange }
-            hl.TSRainbowGreen = { fg = c.green2 }
-            hl.TSRainbowViolet = { fg = c.purple }
-            hl.TSRainbowCyan = { fg = c.cyan }
+    -- {
+    --   "tokyonight.nvim",
+    --   priority = 1000,
+    --   opts = function(_, _opts)
+    --     return {
+    --       style = "moon",
+    --       styles = {
+    --         keywords = { italic = false },
+    --         sidebars = "transparent",
+    --         floats = "transparent",
+    --       },
+    --       transparent = true,
+    --       sidebars = {
+    --         "qf",
+    --         "vista_kind",
+    --         "terminal",
+    --         "spectre_panel",
+    --         "NeogitStatus",
+    --         "startuptime",
+    --         "Outline",
+    --       },
+    --       ---@param hl Highlights
+    --       ---@param c ColorScheme
+    --       on_highlights = function(hl, c)
+    --         hl.CursorLineNr = { fg = c.orange, bold = true }
+    --         -- hl.LineNr = { fg = c.orange, bold = true }
+    --         hl.LineNrAbove = { fg = c.fg_gutter }
+    --         hl.LineNrBelow = { fg = c.fg_gutter }
+    --         local prompt = "#2d3149"
+    --         hl.TelescopeNormal = { bg = c.bg_dark, fg = c.fg_dark }
+    --         hl.TelescopeBorder = { bg = c.bg_dark, fg = c.bg_dark }
+    --         hl.TelescopePromptNormal = { bg = prompt }
+    --         hl.TelescopePromptBorder = { bg = prompt, fg = prompt }
+    --         hl.TelescopePromptTitle = { bg = c.fg_gutter, fg = c.orange }
+    --         hl.TelescopePreviewTitle = { bg = c.bg_dark, fg = c.bg_dark }
+    --         hl.TelescopeResultsTitle = { bg = c.bg_dark, fg = c.bg_dark }
 
-            hl.LspInlayHint = { link = "Comment" }
+    --         hl.TSRainbowRed = { fg = c.red }
+    --         hl.TSRainbowYellow = { fg = c.yellow }
+    --         hl.TSRainbowBlue = { fg = c.blue }
+    --         hl.TSRainbowOrange = { fg = c.orange }
+    --         hl.TSRainbowGreen = { fg = c.green2 }
+    --         hl.TSRainbowViolet = { fg = c.purple }
+    --         hl.TSRainbowCyan = { fg = c.cyan }
 
-            hl["@string.documentation"] = { fg = c.yellow }
-            -- hl["@comment.documentation"] = { fg = "#636da6", italic = false, style = { italic = false } }
+    --         hl.LspInlayHint = { link = "Comment" }
 
-            -- Smali
-            hl["@parameter.builtin"] = { fg = "#efc890" }
+    --         hl["@string.documentation"] = { fg = c.yellow }
+    --         -- hl["@comment.documentation"] = { fg = "#636da6", italic = false, style = { italic = false } }
 
-            -- Rust
-            hl["@lsp.typemod.function.defaultLibrary.rust"] = { link = "@function" }
-            hl["@lsp.typemod.method.defaultLibrary.rust"] = { link = "@method" }
-            hl["@lsp.type.selfTypeKeyword.rust"] = { link = "@variable.builtin" }
-            hl["@lsp.type.decorator.rust"] = { link = "@attribute" }
-            hl["@lsp.type.deriveHelper.rust"] = { link = "@attribute" }
-            hl["@lsp.type.generic.rust"] = { link = "@variable" }
-            hl["@lsp.type.formatSpecifier.rust"] = { link = "@punctuation.special" }
-            hl["@lsp.type.variable.rust"] = { link = "@variable" }
-            hl["@lsp.type.escapeSequence"] = { link = "@string.escape" }
-            hl["@lsp.type.selfKeyword"] = { link = "@variable.builtin" }
-            hl["@lsp.typemod.variable.constant.rust"] = { link = "@constant" }
-            hl["@lsp.typemod.keyword.async.rust"] = { link = "@keyword.coroutine" }
-            hl["@lsp.typemod.enumMember.defaultLibrary"] = { link = "@constant.builtin" }
-            hl["@lsp.typemod.enum.defaultLibrary"] = { link = "@type.builtin" }
-            -- why are these so bad?
-            hl["@lsp.type.keyword.cs"] = {}
-            hl["@lsp.type.keyword.go"] = {}
-            hl["@lsp.type.keyword.rust"] = {}
-            hl["@lsp.type.keyword.zig"] = {}
-            hl["@lsp.type.type.zig"] = {}
-            hl["@lsp.typemod.keyword.injected"] = { link = "@keyword" }
+    --         -- Smali
+    --         hl["@parameter.builtin"] = { fg = "#efc890" }
 
-            hl["@lsp.type.operator"] = { link = "@operator" }
-          end,
-        }
-      end,
-    },
+    --         -- Rust
+    --         hl["@lsp.typemod.function.defaultLibrary.rust"] = { link = "@function" }
+    --         hl["@lsp.typemod.method.defaultLibrary.rust"] = { link = "@method" }
+    --         hl["@lsp.type.selfTypeKeyword.rust"] = { link = "@variable.builtin" }
+    --         hl["@lsp.type.decorator.rust"] = { link = "@attribute" }
+    --         hl["@lsp.type.deriveHelper.rust"] = { link = "@attribute" }
+    --         hl["@lsp.type.generic.rust"] = { link = "@variable" }
+    --         hl["@lsp.type.formatSpecifier.rust"] = { link = "@punctuation.special" }
+    --         hl["@lsp.type.variable.rust"] = { link = "@variable" }
+    --         hl["@lsp.type.escapeSequence"] = { link = "@string.escape" }
+    --         hl["@lsp.type.selfKeyword"] = { link = "@variable.builtin" }
+    --         hl["@lsp.typemod.variable.constant.rust"] = { link = "@constant" }
+    --         hl["@lsp.typemod.keyword.async.rust"] = { link = "@keyword.coroutine" }
+    --         hl["@lsp.typemod.enumMember.defaultLibrary"] = { link = "@constant.builtin" }
+    --         hl["@lsp.typemod.enum.defaultLibrary"] = { link = "@type.builtin" }
+    --         -- why are these so bad?
+    --         hl["@lsp.type.keyword.cs"] = {}
+    --         hl["@lsp.type.keyword.go"] = {}
+    --         hl["@lsp.type.keyword.rust"] = {}
+    --         hl["@lsp.type.keyword.zig"] = {}
+    --         hl["@lsp.type.type.zig"] = {}
+    --         hl["@lsp.typemod.keyword.injected"] = { link = "@keyword" }
+
+    --         hl["@lsp.type.operator"] = { link = "@operator" }
+    --       end,
+    --     }
+    --   end,
+    -- },
 
     -- {
     --   "LazyVim/LazyVim",
@@ -209,4 +211,11 @@ return {
     --   end,
     -- },
     -- },
+
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "catppuccin",
+      },
+    }
   }
