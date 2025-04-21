3c7091fc5b9c8fdd14eb2dee964dec2a3d5500dc
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-04-20 19:15:34 -0600

    testing nginx

--- systems/sdrhub/configuration.nix
+++ systems/sdrhub/configuration.nix
@@ -110,5 +110,17 @@
     };
   };
 
+  services.nginx = {
+    enable = true;
+    virtualHosts.localhost = {
+      locations."/" = {
+        return = "200 '<html><body>It works</body></html>'";
+        extraConfig = ''
+          default_type text/html;
+        '';
+      };
+    };
+  };
+
   system.stateVersion = "24.11"; # Did you read the comment?
 }
