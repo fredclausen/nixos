f53d02323ca0b32b00327019b67306be0cbaf2e5
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-04-20 20:20:34 -0600

    doubt this works

--- systems/sdrhub/configuration.nix
+++ systems/sdrhub/configuration.nix
@@ -134,7 +134,10 @@
       };
 
       locations."dump978" = {
-        proxyPass = "http://192.168.31.20:8083/skyaware/";
+        proxyPass = "http://192.168.31.20:8083/";
+        extraConfig = ''
+          proxy_redirect / /dump978/;
+        '';
       };
     };
   };
