03d95051283bd71de793b27f311959630f42d763
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-11-25 08:14:57 -0700

    clean up darwin comments

--- macflake/darwin/default.nix
+++ macflake/darwin/default.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }:
 {
-  # here go the darwin preferences and config items
   environment = {
     systemPackages = [ pkgs.coreutils ];
     systemPath = [ "/opt/homebrew/bin" ];
@@ -14,54 +13,14 @@
 
   system = {
     primaryUser = "fred";
-    #     keyboard = {
-    #       enableKeyMapping = true;
-    #       remapCapsLockToEscape = true;
-    #     };
-    #     defaults = {
-    #       finder = {
-    #         AppleShowAllExtensions = true;
-    #         _FXShowPosixPathInTitle = true;
-    #         CreateDesktop = false;
-    #         FXPreferredViewStyle = "Nlsv";
-    #       };
-    #       dock = {
-    #         autohide = true;
-    #         autohide-delay = 1.0e-2;
-    #         autohide-time-modifier = 1.0e-2;
-    #         show-recents = false;
-    #       };
-    #       CustomSystemPreferences = {
-    #         NSGlobalDomain = { NSWindowShouldDragOnGesture = true; };
-    #       };
-    #       NSGlobalDomain = {
-    #         "com.apple.sound.beep.feedback" = 0;
-    #         "com.apple.sound.beep.volume" = 0.0;
-    #         AppleKeyboardUIMode = 3;
-    #         ApplePressAndHoldEnabled = false;
-    #         InitialKeyRepeat = 15;
-    #         KeyRepeat = 2;
-    #         AppleShowAllExtensions = true;
-    #         NSWindowResizeTime = 0.1;
-    #         NSAutomaticWindowAnimationsEnabled = false;
-    #       };
-    #     };
     stateVersion = 6;
-    #     activationScripts = {
-    #       postActivation = {
-    #         text = "sudo chsh -s ${pkgs.bashInteractive}/bin/bash";
-    #       };
-    #     };
   };
   security.pam.services.sudo_local.touchIdAuth = true;
   users.users.fred.home = "/Users/fred";
-  #   fonts.packages = [ pkgs.nerd-fonts.meslo-lg ];
-  #   ids.gids.nixbld = 350;
 
   homebrew = {
     enable = true;
     caskArgs.no_quarantine = true;
-    # use home brew to install packages for spotlight to work
     taps = [
       "pothosware/pothos"
     ];
