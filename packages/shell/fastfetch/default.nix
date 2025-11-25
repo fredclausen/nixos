acd399e448818286efcadfc31421e7323bbdd782
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-11-25 09:30:09 -0700

    remove fastfetch json

--- packages/shell/fastfetch/default.nix
+++ packages/shell/fastfetch/default.nix
@@ -5,22 +5,60 @@
   user,
   ...
 }:
+
 let
   username = user;
 in
 {
   config = {
     home-manager.users.${username} = {
-      home.packages = with pkgs; [
-        fastfetch
-      ];
+
+      home.packages = [ pkgs.fastfetch ];
 
       programs.fastfetch = {
         enable = true;
-        settings = (
-          builtins.fromJSON (builtins.readFile ../../../dotfiles/fred/.config/fastfetch/config.jsonc)
-        );
+
+        settings = {
+          "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
+
+          modules = [
+            "title"
+            "separator"
+            "os"
+
+            {
+              type = "host";
+              format = "{/2}{-}{/}{2}{?3} {3}{?}";
+            }
+
+            "kernel"
+            "uptime"
+
+            {
+              type = "battery";
+              format = "{/4}{-}{/}{4}{?5} [{5}]{?}";
+            }
+
+            "break"
+            "packages"
+            "shell"
+            "display"
+            "terminal"
+            "break"
+            "cpu"
+
+            {
+              type = "gpu";
+              key = "GPU";
+            }
+
+            "memory"
+            "break"
+            "colors"
+          ];
+        };
       };
+
     };
   };
 }
