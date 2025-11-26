ebf3da5dd32474157f0dd485a71030b540611061
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-11-26 11:15:12 -0700

    add authorized key

--- modules/common/sops.nix
+++ modules/common/sops.nix
@@ -46,6 +46,11 @@ in
       owner = username;
       mode = "0600";
     };
+    secrets."ssh/authorized_keys" = {
+      path = "${homeDir}/.ssh/authorized_keys";
+      owner = username;
+      mode = "0600";
+    };
   };
 
   users.users.${username}.openssh.authorizedKeys.keys = [
@@ -53,3 +58,12 @@ in
     config.sops.secrets."ssh/id_ed25519.pub".path
   ];
 }
+
+## This is the flow for adding a new system:
+# 1. Clone this repository to the new system.
+# 2. Run `add_new_system_sop.sh` to generate new age keys and SSH keys ON THE NEW SYSTEM. This is safe for a new system.
+# 3. Add the new age public key to `.sops.yaml`. This needs to be done on a system that already has access to the secrets.
+# 4. Re-encrypt the secrets with the updated keys `sops updatekeys secrets.yaml`
+# 5. Commit the updated `.sops.yaml` and `secrets.yaml` files.
+# 6. Push the changes to the repository.
+# 7. On the new system, pull the latest changes from the repository.
