e20875427e642cdc59114ab9c5c1bbc8b7399ec8
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-11-27 12:58:19 -0700

    hmmm

--- modules/adsb-docker-units.nix
+++ modules/adsb-docker-units.nix
@@ -54,7 +54,7 @@ let
             "run"
             "--name"
             c.name
-            "--privileged"
+            "--network adsbnet"
           ]
           + " ${ttyFlag} ${envFlags} ${envFileFlags} ${volumeFlags} ${tmpfsFlags} ${portFlags} "
           + (c.extraDockerArgs or "")
