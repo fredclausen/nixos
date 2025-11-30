ae2453f3341ab78a37aea344457d25995b9325ec
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-11-29 17:38:01 -0700

    testing

--- packages/common/system/default.nix
+++ packages/common/system/default.nix
@@ -45,6 +45,16 @@
       airspy
     ];
 
+    services.avahi = {
+      enable = true;
+      nssmdns4 = true;
+      publish = {
+        enable = true;
+        addresses = true;
+        workstation = true;
+      };
+    };
+
     xdg.portal = {
       enable = true;
       config.common.default = "*";
