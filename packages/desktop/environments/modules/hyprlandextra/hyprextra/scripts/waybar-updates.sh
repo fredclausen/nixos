3407683216f9c05971dc52bfd55b980201ea67c2
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-12-14 17:19:38 -0700

    More Waybar stuff

--- packages/desktop/environments/modules/hyprlandextra/hyprextra/scripts/waybar-updates.sh
+++ packages/desktop/environments/modules/hyprlandextra/hyprextra/scripts/waybar-updates.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if nixos-needsreboot >/dev/null 2>&1; then
+if nixos-needsreboot --dry-run >/dev/null 2>&1; then
     echo '{"text":"󰜉","class":"reboot","tooltip":"Reboot required"}'
     exit 0
 fi
