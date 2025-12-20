1d5718edad9cff60e991b57c8dab806799922d10
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2025-12-19 18:23:04 -0700

    Cooler fast fetch

--- packages/shell/fastfetch/default.nix
+++ packages/shell/fastfetch/default.nix
@@ -16,47 +16,8 @@ in
       programs.fastfetch = {
         enable = true;
 
-        settings = {
-          "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
-
-          modules = [
-            "title"
-            "separator"
-            "os"
-
-            {
-              type = "host";
-              format = "{/2}{-}{/}{2}{?3} {3}{?}";
-            }
-
-            "kernel"
-            "uptime"
-
-            {
-              type = "battery";
-              format = "{/4}{-}{/}{4}{?5} [{5}]{?}";
-            }
-
-            "break"
-            "packages"
-            "shell"
-            "display"
-            "terminal"
-            "break"
-            "cpu"
-
-            {
-              type = "gpu";
-              key = "GPU";
-            }
-
-            "memory"
-            "break"
-            "colors"
-          ];
-        };
+        settings = builtins.fromJSON (builtins.readFile ./config.json);
       };
-
     };
   };
 }
