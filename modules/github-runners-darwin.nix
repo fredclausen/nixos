2933352e90ef113a36e87b5a2b2542cbaf52cc8e
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2026-01-06 10:23:27 -0700

    tyr

--- modules/github-runners-darwin.nix
+++ modules/github-runners-darwin.nix
@@ -95,7 +95,8 @@ let
         echo "=== runner ${id} starting at $(date) ==="
 
         # Ensure HOME is correct even if launchd hands us something odd.
-        USER_HOME="$(dscl . -read /Users/${escapeShellArg cfg.user} NFSHomeDirectory | ${pkgs.gawk}/bin/awk '{print $2}')"
+        USER_HOME="/Users/fred"
+        #USER_HOME="$(dscl . -read /Users/${escapeShellArg cfg.user} NFSHomeDirectory | ${pkgs.gawk}/bin/awk '{print $2}')"
         export HOME="$USER_HOME"
         echo "HOME=$HOME"
 
